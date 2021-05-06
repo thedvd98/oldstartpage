308dc10a54e4aeec2f8a68e32273ee32b859ae1d
thedvd98 <gianadavide@gmail.com>
2021-05-06 21:05:45 +0200

    new area

--- genera.sh
+++ genera.sh
@@ -10,21 +10,32 @@ function add()
 	echo -ne $1 >> $INDEX
 }
 
-add '<div class="main">'
-add '<div class="url_list">'
+function add_url_list()
+{
+	add '<div class="url_list">\n'
+	while iFS="" read -r line || [ -n "$line" ]
+	do
+		title=$(echo $line | cut -d' ' -f1)
+		url=$(echo $line | cut -d' ' -f2)
+		add '\t\t<a href="'
+		add $url
+		add '">'
+		add $title
+		add '</a><br/>\n'
+	done < $DATA
 
-while iFS="" read -r line || [ -n "$line" ]
-do
-	title=$(echo $line | cut -d' ' -f1)
-	url=$(echo $line | cut -d' ' -f2)
-	add '\t\t<a href="'
-	add $url
-	add '">'
-	add $title
-	add '</a><br/>\n'
-done < $DATA
+	add '</div>'
+}
 
-add '</div>'
+function add_pages()
+{
+	add '<div class="page_list">'
+	add '</div>'
+}
+
+add '<div class="main">\n'
+add_pages
+add_url_list
 add '</div>'
 
 cat footer.html >> $INDEX
